8a06257b8388f7b155b89470e251d61d85bf7eb7
Thanh Linh <75314425+UIT-20521535@users.noreply.github.com>
2021-12-20 15:39:07 +0700

    hoàn thành

--- snakegame.cpp
+++ snakegame.cpp
@@ -265,3 +265,84 @@ void showStartMenu()
 		exit(1);
 }
 #pragma endregion
+#pragma region SnakeFunction
+// vẽ 1 phần của rắn
+void drawSnakePart(Point p)
+{
+	gotoxy(p.x, p.y);
+	cout << BODY;
+}
+
+// vẽ toàn bộ rắn
+void drawSnake()
+{
+	for (size_t i = 0; i < snake.size(); i++)
+		drawSnakePart(snake[i]);
+}
+
+// di chuyển rắn
+void move()
+{
+	prevTail = snake.back();
+	for (size_t i = snake.size() - 1; i > 0; i--)
+		snake[i] = snake[i - 1];
+	if (direction == Direction::up)
+		snake[0].y -= 1;
+	else if (direction == Direction::down)
+		snake[0].y += 1;
+	else if (direction == Direction::left)
+		snake[0].x -= 1;
+	else if (direction == Direction::right)
+		snake[0].x += 1;
+}
+
+// vẽ lại đầu và đuôi rắn để làm nó di chuyển
+void drawHeadnTail()
+{
+	gotoxy(snake[0].x, snake[0].y);
+	cout << BODY;
+	gotoxy(prevTail.x, prevTail.y);
+	cout << ' '; // xóa đuôi cũ
+}
+
+// Check if rắn ăn chính nó
+bool isBiteItself()
+{
+	Point head = snake[0];
+	for (size_t i = 1; i < snake.size(); i++)
+		if (head.x == snake[i].x && head.y == snake[i].y)
+			return true;
+	return false;
+}
+
+// Tăng độ dài khi ăn quả
+void growing()
+{
+	snake.push_back(prevTail);
+}
+#pragma endregion
+
+
+#pragma region ConsoleFunction
+// đến địa điểm có tọa độ x, y
+void gotoxy(int x, int y)
+{
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(
+		GetStdHandle(STD_OUTPUT_HANDLE),
+		coord
+	);
+}
+
+// đặt khả năng hiển thị của con trỏ
+void ShowConsoleCursor(bool showFlag)
+{
+	HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO cursorInfo;
+	GetConsoleCursorInfo(out, &cursorInfo);
+	cursorInfo.bVisible = showFlag;
+	SetConsoleCursorInfo(out, &cursorInfo);
+}
+#pragma endregion
