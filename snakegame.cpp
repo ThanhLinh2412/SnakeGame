4abb0aed8bdc177632a1cae218e1cbd402211f19
Thanh Linh <75314425+UIT-20521535@users.noreply.github.com>
2021-12-20 15:35:49 +0700

    cập nhật version 0.1

--- snakegame.cpp
+++ snakegame.cpp
@@ -74,4 +74,194 @@ void startGame();
 void resetSnake();
 void showStartMenu();
 #pragma endregion
+/*
+ * Hàm main
+ */
+int main()
+{
+	showStartMenu();
+	return 0;
+}
 
+
+#pragma region GameFunction
+// Vẽ khung trò chơi
+void drawBox()
+{
+	for (size_t i = 0; i < WIDTH; i++)
+		cout << '=';
+	gotoxy(0, HEIGHT);
+	for (size_t i = 0; i < WIDTH; i++)
+		cout << '=';
+	for (size_t i = 1; i < HEIGHT; i++)
+	{
+		gotoxy(0, i);
+		cout << '|';
+	}
+	for (size_t i = 1; i < HEIGHT; i++)
+	{
+		gotoxy(WIDTH, i);
+		cout << '|';
+	}
+}
+
+// Kiểm tra nếu đụng vào tường
+bool isHitWall()
+{
+	return snake[0].x == 0 || snake[0].y == 0 || snake[0].x == WIDTH || snake[0].y == HEIGHT;
+}
+
+// Tạo quả trên bàn cờ
+void genApple()
+{
+	srand(time(0));
+	int x = rand() % (WIDTH - 1) + 1;
+	int y = rand() % (HEIGHT - 1) + 1;
+	apple = {
+		x,
+		y,
+	};
+	gotoxy(x, y);
+	cout << APPLE;
+}
+
+// Kiểm tra nếu rắn đã ăn quả
+bool isAteApple()
+{
+	return snake[0].x == apple.x && snake[0].y == apple.y;
+}
+
+// Hiện điểm
+void displayScore()
+{
+	gotoxy(WIDTH + 5, 2);
+	cout << "Your score: " << score;
+}
+
+
+// Hiện menu
+void showEndMenu()
+{
+	system("cls");
+	gotoxy(0, 0);
+	cout << "End game!" << endl;
+	cout << "Your score: " << score << endl;
+	cout << "Do you want to play again ([y]/[n]): ";
+	char option;
+	cin >> option;
+	option = tolower(option);
+	if (option == 'y')
+	{
+		resetSnake();
+		startGame();
+	}
+	else if (option == 'n')
+		exit(1);
+}
+
+void startGame()
+{
+	system("cls");
+	ShowConsoleCursor(false);
+
+	drawBox();
+	drawSnake();
+	genApple();
+	displayScore();
+
+	while (true)
+	{
+		if (_kbhit())
+		{
+			char ch = _getch();
+			ch = tolower(ch);
+			if (ch == 'a' && direction != Direction::right)
+				direction = Direction::left;
+			else if (ch == 'w' && direction != Direction::down)
+				direction = Direction::up;
+			else if (ch == 's' && direction != Direction::up)
+				direction = Direction::down;
+			else if (ch == 'd' && direction != Direction::left)
+				direction = Direction::right;
+			else if (ch == 'q') // Quit game
+			{
+				showEndMenu();
+				break;
+			}
+		}
+		move();
+		drawHeadnTail();
+		if (isAteApple())
+		{
+			score++;
+			displayScore();
+			growing();
+			genApple();
+		}
+		if (isBiteItself())
+		{
+			ShowConsoleCursor(true);
+			showEndMenu();
+			break;
+		}
+		if (isHitWall())
+		{
+			ShowConsoleCursor(true);
+			showEndMenu();
+			break;
+		}
+		Sleep(speed);
+	}
+}
+
+void resetSnake()
+{
+	score = 0;
+	direction = Direction::right;
+	snake = {
+		Point{ WIDTH / 2 + 2, HEIGHT / 2 },
+		Point{ WIDTH / 2 + 1, HEIGHT / 2 },
+		Point{ WIDTH / 2, HEIGHT / 2 },
+		Point{ WIDTH / 2 - 1, HEIGHT / 2 },
+		Point{ WIDTH / 2 - 2, HEIGHT / 2 }
+	};
+}
+
+// Màn hình bắt đầu
+void showStartMenu()
+{
+	system("cls");
+	cout << "Welcome to snake game!" << endl;
+	cout << "Sự lựa chọn:" << endl;
+	cout << "1. Bắt đầu" << endl;
+	cout << "2. Kết thúc" << endl;
+	cout << "Bạn lựa chọn: ";
+	int option;
+	cin >> option;
+	if (option == 1)
+	{
+		system("cls");
+		cout << "Chọn level (1 - 5): ";
+		int t;
+		cin >> t;
+		speed = 600 - t * 100; // Calculate speed depend on level
+		system("cls");
+		cout << "Tip: Trong khi đang chơi, bạn có thể bấm 'q' để thoát.";
+		gotoxy(0, 3);
+		cout << "Sẵn sàng!";
+		Sleep(1000);
+		for (size_t i = 3; i > 0; i--)
+		{
+			gotoxy(0, 3);
+			cout << i << "         ";
+			Sleep(1000);
+		}
+		gotoxy(0, 3);
+		cout << "BẮT ĐẦU!";
+		Sleep(1000);
+		startGame();
+	}
+	else if (option == 2)
+		exit(1);
+}
+#pragma endregion
